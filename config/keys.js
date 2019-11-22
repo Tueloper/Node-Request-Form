b1db33f74f0c454866397b90806cf3043d95f8af
Tueloper <ozurumbatochukwu@yahoo.com>
2019-11-22 14:05:21 +0100

    BE/Request from app init

---
+++ config/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+  mongoURI: "mongodb://127.0.0.1:27017/tueLop_dev_db",
+  cookieKey: "Tue123123",
+  redisUrl: "redis://127.0.0.1:6379",
+  SECRET: "Tuenial9873",
+  EMAIL_HOST: "smtp.gmail.com",
+  EMAIL_ID: 'tozo2345@gmail.com',
+  EMAIL_PASS: '#r78sh35'
+};
