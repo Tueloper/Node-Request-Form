e0a506a93917c5b34743b221c904517009460ff5
miracle-E <50963919+miracle-E@users.noreply.github.com>
2019-12-05 13:02:55 +0100

    form added

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-const mongoose = require('mongoose');
+const mongoose = require('mongoose')
 const keys = require('./../config/keys');
 const chalk = require('chalk')
 
@@ -21,7 +21,7 @@ mongoose.connect(
     useFindAndModify: false,
     useUnifiedTopology: true
   },
-  function(err, client) {
+  function (err, client) {
     if (err) console.log(chalk.redBright("Database Connection Failed"));
     if (client)
       console.log(chalk.gray.inverse.italic("Database Connection Passed"));
@@ -47,15 +47,15 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api/v1', [indexRouter, usersRouter]);
+app.use('/', [indexRouter, usersRouter]);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
